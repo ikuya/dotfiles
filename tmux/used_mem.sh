5e158e69003fa02a89e9b201669488ca0bbd3643
usobuku <if@ifmi.local>
2012-06-17 22:58:33 +0900

    mod for linux

--- tmux/used_mem.sh
+++ tmux/used_mem.sh
@@ -3,37 +3,43 @@
 ## メモリ使用率を表示する
 # http://d.hatena.ne.jp/yonchu/20120414/1334422075
 
-## vm_statコマンドのチェック
-if ! type vm_stat > /dev/null 2>&1; then
-    exit 1
-fi
-
 #メモリ使用率を求める
 calculate_used_mem() {
-    # vm_stat
-    # page size of 4096 bytes
-    VM_STAT=`vm_stat`
-    PAGES_FREE=$(echo "$VM_STAT" | awk '/Pages free/ {print $3}' | tr -d '.')
-    PAGES_ACTIVE=$(echo "$VM_STAT" | awk '/Pages active/ {print $3}' | tr -d '.')
-    PAGES_INACTIVE=$(echo "$VM_STAT" | awk '/Pages inactive/ {print $3}' | tr -d '.')
-    PAGES_SPECULATIVE=$(echo "$VM_STAT" | awk '/Pages speculative/ {print $3}' | tr -d '.')
-    PAGES_WIRED=$(echo "$VM_STAT" | awk '/Pages wired down/ {print $4}' | tr -d '.')
+    if ! type vm_stat > /dev/null 2>&1; then
+        #free
+        FREE=`free`
+        MEM_FREE=$(echo "$FREE" |awk '/Mem:/ {print $4}')
+        MEM_USED=$(echo "$FREE" |awk '/Mem:/ {print $3}')
+        MEM_TOTAL=$(echo "$FREE" |awk '/Mem:/ {print $2}')
 
-    # 空きメモリ
-    FREE_MEM=$(($PAGES_FREE + $PAGES_SPECULATIVE))
+        #使用中メモリ
+        USED_MEM_PERCENT_BY_FREE_COMMAND=$(echo `echo "scale=4; $MEM_USED / $MEM_TOTAL * 100" | bc | sed -e 's/^\(....\).*/\1/'`)
+        echo "MEM:${USED_MEM_PERCENT_BY_FREE_COMMAND}%"
+    else
+    	# vm_stat
+    	# page size of 4096 bytes
+        VM_STAT=`vm_stat`
+        PAGES_FREE=$(echo "$VM_STAT" | awk '/Pages free/ {print $3}' | tr -d '.')
+        PAGES_ACTIVE=$(echo "$VM_STAT" | awk '/Pages active/ {print $3}' | tr -d '.')
+        PAGES_INACTIVE=$(echo "$VM_STAT" | awk '/Pages inactive/ {print $3}' | tr -d '.')
+        PAGES_SPECULATIVE=$(echo "$VM_STAT" | awk '/Pages speculative/ {print $3}' | tr -d '.')
+        PAGES_WIRED=$(echo "$VM_STAT" | awk '/Pages wired down/ {print $4}' | tr -d '.')
 
-    # 使用中メモリ
-    USED_MEM=$(($PAGES_ACTIVE + $PAGES_INACTIVE + $PAGES_WIRED))
+    	# 空きメモリ
+        FREE_MEM=$(($PAGES_FREE + $PAGES_SPECULATIVE))
 
-    # 合計
-    TOTAL_MEM=$(($FREE_MEM + $USED_MEM))
+    	# 使用中メモリ
+        USED_MEM=$(($PAGES_ACTIVE + $PAGES_INACTIVE + $PAGES_WIRED))
+
+    	# 合計
+        TOTAL_MEM=$(($FREE_MEM + $USED_MEM))
 
 
-    # 使用中メモリ(%)
-    #  小数点第1位まで求めて後から小数点文字(ドット)を挿入
-    USED_MEM_PERCENT=$(echo "$(($USED_MEM * 1000 / $TOTAL_MEM))" | sed -e 's/\(.*\)\([0-9]\)/\1.\2/' -e 's/^\./0./')
-    echo "${USED_MEM_PERCENT}%"
-
+    	# 使用中メモリ(%)
+    	#  小数点第1位まで求めて後から小数点文字(ドット)を挿入
+        USED_MEM_PERCENT=$(echo "$(($USED_MEM * 1000 / $TOTAL_MEM))" | sed -e 's/\(.*\)\([0-9]\)/\1.\2/' -e 's/^\./0./')
+        echo "Mem:${USED_MEM_PERCENT}%"
+    fi
     # RET
     return 0
 }
