258f98e540d4caed4de10c9a30ea68750be7620b
usobuku <if@am31811.(none)>
2012-06-24 01:51:23 +0900

    delete-tailline-whitespace

--- .emacs.d/conf/04_hook.el
+++ .emacs.d/conf/04_hook.el
@@ -13,3 +13,6 @@
     (turn-on-eldoc-mode)))
 ;; elacs-lisp-modeのhookをセット
 (add-hook 'emacs-lisp-mode-hook 'elisp-mode-hooks)
+
+;; ファイルの保存時に行末の空白文字を削除
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
