be1ea10232b74d7dc48f3a1c0d9dfe70a312181f
usobuku <if@ifmi.local>
2012-07-21 15:06:05 +0900

    c-mode conf

---
+++ .emacs.d/conf/22_c.el
@@ -0,0 +1,15 @@
+;; c-mode, c++-mode
+
+(add-hook 'c-mode-hook
+          '(lambda()
+             (c-set-style "k&r")
+             (setq indent-tabs-mode t)
+             (setq c-basic-offset 4)))
+
+;; 拡張子とmodeの対応
+(setq auto-mode-alist
+      (append
+       '(("\\.c$" . c-mode))
+       '(("\\.h$" . c-mode))
+       '(("\\.cpp$". c++-mode))
+       auto-mode-alist))
