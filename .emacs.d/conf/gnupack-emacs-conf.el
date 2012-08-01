e10063aabf02dea2560a107298d0c920f48260e7
usobuku <ikuya@LENOVO-2B083251.(none)>
2012-08-01 19:17:14 +0900

    frame alpha

--- .emacs.d/conf/gnupack-emacs-conf.el
+++ .emacs.d/conf/gnupack-emacs-conf.el
@@ -175,10 +175,10 @@
 ;; @ buffer
 
    ;; バッファ画面外文字の切り詰め表示
-   (setq truncate-lines nil)
+   (setq truncate-lines t)
 
    ;; ウィンドウ縦分割時のバッファ画面外文字の切り詰め表示
-   (setq truncate-partial-width-windows t)
+   (setq truncate-partial-width-windows nil)
 
    ;; 同一バッファ名にディレクトリ付与
    (require 'uniquify)
@@ -496,6 +496,9 @@
    ;; (color-theme-initialize)
 
 ;; ------------------------------------------------------------------------
+;; ウィンドウの透明化
+(add-to-list 'default-frame-alist '(alpha . (0.90 0.90)))
+
 ;; ツールバーを非表示
 (tool-bar-mode -1)
 ;; スクロールバーを非表示
