820c22f64ab0a58f9b9781aec648f027b7dd0be9
usobuku <if@kiritsubo.local>
2013-03-09 01:50:18 +0900

    changed: anything to elpa packages

--- .emacs.d/conf/10_elisp.el
+++ .emacs.d/conf/10_elisp.el
@@ -186,3 +186,9 @@
 ;; ---------- ispell ----------
 (require 'ispell nil t)
 (setq-default ispell-program-name "aspell")
+
+;; ---------- elpa ----------
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
