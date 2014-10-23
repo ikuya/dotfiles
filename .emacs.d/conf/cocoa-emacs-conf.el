f58ba02b3ee4e4a825ff3fb865181b036c8cc350
ikuya <if@ifmi.local>
2014-10-24 00:59:21 +0900

    add conf for live preview in Markdown mode via Markd 2 app.

--- .emacs.d/conf/cocoa-emacs-conf.el
+++ .emacs.d/conf/cocoa-emacs-conf.el
@@ -173,3 +173,13 @@
 ;                                              (interactive)
 ;                                              (dic-lookup-w3m "thesaurus-webster")))
 )
+
+;; Live preview in Markdown mode via Markd2.app
+(defun markdown-preview-file ()
+  "run Marked on the current file and revert the buffer"
+  (interactive)
+  (shell-command 
+   (format "open -a /Applications/Marked\\ 2.app %s" 
+       (shell-quote-argument (buffer-file-name))))
+  )
+(global-set-key (kbd "C-c m") 'markdown-preview-file)
