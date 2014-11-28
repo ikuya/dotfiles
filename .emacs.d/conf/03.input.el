e6f6b88c51ae14e15afb90685c4e9a97618c555f
ikuya <ikuya@ssag>
2014-11-28 16:23:55 +0900

    [modify] M-f の改良
    「次の単語の直前のスペース」ではなく, 「次の単語の先頭」に移動する

--- .emacs.d/conf/03.input.el
+++ .emacs.d/conf/03.input.el
@@ -9,3 +9,12 @@
 (setq next-line-add-newlines nil)
 ;; Automatic character pairing (e.g. parenthesis)
 (electric-pair-mode t)
+
+;; M-f の改良. 「次の単語の直前のスペース」ではなく, 「次の単語の先頭」に移動する
+(defun next-word(p)
+  "Move point to the beginning of the next word, past any spaces"
+  (interactive "d")
+  (forward-word)
+  (forward-word)
+  (backward-word))
+(global-set-key (kbd "M-f") 'next-word)
