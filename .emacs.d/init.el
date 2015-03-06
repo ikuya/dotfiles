a5fa96bf09184e6cf769e7ef4f89c3c3ada86062
ikuya <if@ifma.local>
2015-03-06 23:01:08 +0900

    [delete] init.elからELPAの設定を削除

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,18 +14,6 @@
 ;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
 (add-to-load-path "elisp" "conf" "public_repos")
 
-;; ========== ELPA ==========
-;;; package (emacs24では削除 - .emacs.d/elispのpackage.el[c]も削除)
-;;; ELPAの利用
-;; http://bit.ly/pkg-el23
-(when (require 'package nil t)
-  ;; パッケージリポジトリにmarmaladeと開発者運営のELPAを追加
-  (add-to-list 'package-archives
-               '("marmalade" . "http://marmalade-repo.org/packages/"))
-  (add-to-list 'package-archives
-               '("ELPA" . "http://tromey.com/elpa/"))
-  (package-initialize)) ; .emacs.d/elpaにインストールされたパッケージを読み込む
-
 ;; ========== Conf 読み込み ==========
 ;; init-loader.elを使用
 (require 'init-loader)
