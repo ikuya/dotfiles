d59c06a4a6c793b08ed659f174bba8357db834e0
ikuya <ikuya@ssag>
2015-03-18 14:10:38 +0900

    [modify] whitespace-modeの機能を一部無効化
    タブとかスペースとか全部可視化していたのはやり過ぎだった。
    保存時の無駄スペース削除機能を残してひとまずコメントアウト

--- .emacs.d/conf/02_window.el
+++ .emacs.d/conf/02_window.el
@@ -38,7 +38,7 @@
 ;; 入力中のマウスカーソルが邪魔
 ;; banish: 右上隅に移動; exile: 右上隅に移動(しばらくすると元に戻る
 ;; jump: ランダムに移動; animate: ランダムに移動(アニメーション) none:移動しない
-(if (display-mouse-p) (mouse-avoidance-mode 'banish))
+;(if (display-mouse-p) (mouse-avoidance-mode 'banish))
 
 ;; ========== MODE LINE ==========
 ;; 行番号
@@ -139,23 +139,23 @@
 ;; ---------- Whitespace mode ----------
 ;; 空白, タブ文字を可視化する
 (require 'whitespace)
-(setq whitespace-style '(face           ; faceで可視化
-                         trailing       ; 行末
-                         tabs           ; タブ
-                         spaces         ; スペース
-                         empty          ; 先頭/末尾の空行
-                         space-mark     ; 表示のマッピング
-                         tab-mark
-                         ))
+;; (setq whitespace-style '(face           ; faceで可視化
+;;                          trailing       ; 行末
+;;                          tabs           ; タブ
+;;                          spaces         ; スペース
+;;                          empty          ; 先頭/末尾の空行
+;;                          space-mark     ; 表示のマッピング
+;;                          tab-mark
+;;                          ))
 
-(setq whitespace-display-mappings
-      '((space-mark ?\u3000 [?\u25a1])
-        ;; WARNING: the mapping below has a problem.
-        ;; When a TAB occupies exactly one column, it will display the
-        ;; character ?\xBB at that column followed by a TAB which goes to
-        ;; the next TAB column.
-        ;; If this is a problem for you, please, comment the line below.
-        (tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
+;; (setq whitespace-display-mappings
+;;       '((space-mark ?\u3000 [?\u25a1])
+;;         ;; WARNING: the mapping below has a problem.
+;;         ;; When a TAB occupies exactly one column, it will display the
+;;         ;; character ?\xBB at that column followed by a TAB which goes to
+;;         ;; the next TAB column.
+;;         ;; If this is a problem for you, please, comment the line below.
+;;         (tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
 
 ;; スペースは全角のみを可視化
 (setq whitespace-space-regexp "\\(\u3000+\\)")
@@ -163,20 +163,20 @@
 ;; 保存前に自動でクリーンアップ
 (setq whitespace-action '(auto-cleanup))
 
-(global-whitespace-mode 1)
+;; (global-whitespace-mode 1)
 
-(defvar my/bg-color "#232323")
-(set-face-attribute 'whitespace-trailing nil
-                    :background my/bg-color
-                    :foreground "DeepPink"
-                    :underline t)
-(set-face-attribute 'whitespace-tab nil
-                    :background my/bg-color
-                    :foreground "LightSkyBlue"
-                    :underline t)
-(set-face-attribute 'whitespace-space nil
-                    :background my/bg-color
-                    :foreground "GreenYellow"
-                    :weight 'bold)
-(set-face-attribute 'whitespace-empty nil
-                    :background my/bg-color)
+;; (defvar my/bg-color "#232323")
+;; (set-face-attribute 'whitespace-trailing nil
+;;                     :background my/bg-color
+;;                     :foreground "DeepPink"
+;;                     :underline t)
+;; (set-face-attribute 'whitespace-tab nil
+;;                     :background my/bg-color
+;;                     :foreground "LightSkyBlue"
+;;                     :underline t)
+;; (set-face-attribute 'whitespace-space nil
+;;                     :background my/bg-color
+;;                     :foreground "GreenYellow"
+;;                     :weight 'bold)
+;; (set-face-attribute 'whitespace-empty nil
+;;                     :background my/bg-color)
