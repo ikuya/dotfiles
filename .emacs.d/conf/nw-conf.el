827f38bc109111bdcd126170af9a81346d3b3f2d
ikuya <ikuya@somewhere>
2017-08-16 08:44:06 +0000

    Update nw-conf.el
    Change prefix key of Elscreen

--- .emacs.d/conf/nw-conf.el
+++ .emacs.d/conf/nw-conf.el
@@ -29,7 +29,7 @@
 ;; ----------  ElScreen ----------
 (require 'elscreen nil t)
 ;; ElScreenのプレフィックス(default: C-z)
-(elscreen-set-prefix-key (kbd "C-z"))
+(elscreen-set-prefix-key (kbd "C-t"))
 (when (require 'elscreen nil t)
   ; C-z C-zをタイプした場合にデフォルトのC-zを利用する
   (if window-system
