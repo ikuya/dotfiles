2da82d2017bf8e1bbbcbaf4e3eab0e098df9b45b
ikuya <i.rokujo@gmail.com>
2017-10-06 23:16:48 +0900

    Add config.sh

---
+++ config.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+DOTFILE_DIR=`dirname $0`
+cd $DOTFILE_DIR
+
+git submodule update --init --recursive
+
+mkdir $HOME/bin
+
+## Symbolic link
+ln -s $DOTFILE_DIR/.emacs.d $HOME/.emacs.d
+ln -s $DOTFILE_DIR/zsh/.zshrc $HOME/.zshrc
+ln -s $DOTFILE_DIR/git/.gitconfig $HOME/.gitconfig
+ln -s $DOTFILE_DIR/tmux/.tmux.conf $HOME/.tmux.conf
+ln -s $DOTFILE_DIR/tmux/loadaverage.sh $HOME/bin/loadaverage.sh
+ln -s $DOTFILE_DIR/tmux/used_mem.sh $HOME/bin/used_mem.sh
+ln -s $DOTFILE_DIR/misc/.inputrc $HOME/.inputrc
+ln -s $DOTFILE_DIR/misc/.screenrc $HOME/.screenrc
+
+## Emacs
+cd $DOTFILE_DIR/.emacs.d/elisp/helm
+EMACSLOADPATH="$DOTFILE_DIR/.emacs.d/elisp/emacs-async:"
+make
