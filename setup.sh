9ef5b7a2ea565ca2dbacde3ade677df1d8388026
ikuya <i.rokujo@gmail.com>
2017-10-14 21:15:10 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,51 @@
 #!/bin/sh
 
-DOTFILE_DIR=$(cd $(dirname $0); pwd)
-cd $DOTFILE_DIR
+DOTFILES=$(cd $(dirname $0); pwd)
+EMACSD=$DOTFILES/.emacs.d
+ZSH=$DOTFILES/zsh
+TMUX=$DOTFILES/tmux
+GIT=$DOTFILES/git
+MISC=$DOTFILES/misc
 
+deploy() {
+    if [ ! -e $2 ]; then
+        case $1 in
+            "m") mkdir $2 ;;
+            "t") touch $2 ;;
+            "l") ln -s $3 $2 ;;
+        esac
+    fi
+}
+
+# Submodules initialization
+cd $DOTFILES
 git submodule update --init --recursive
 
-mkdir $HOME/bin
-
-## Symbolic link
-ln -s $DOTFILE_DIR/.emacs.d $HOME/.emacs.d
-ln -s $DOTFILE_DIR/zsh/.zshrc $HOME/.zshrc
-ln -s $DOTFILE_DIR/git/.gitconfig $HOME/.gitconfig
-ln -s $DOTFILE_DIR/tmux/.tmux.conf $HOME/.tmux.conf
-ln -s $DOTFILE_DIR/tmux/loadaverage.sh $HOME/bin/loadaverage.sh
-ln -s $DOTFILE_DIR/tmux/used_mem.sh $HOME/bin/used_mem.sh
-ln -s $DOTFILE_DIR/misc/.inputrc $HOME/.inputrc
-ln -s $DOTFILE_DIR/misc/.screenrc $HOME/.screenrc
-
-## Emacs
-touch $DOTFILE_DIR/.emacs.d/.scratch-log-prev
-touch $DOTFILE_DIR/.emacs.d/.scratch-log
-
-echo "export PATH=$DOTFILE_DIR/.emacs.d/.cask/bin:$PATH" >> $HOME/.zlogin
-cd $DOTFILE_DIR/.emacs.d
-#cask init  # Caskファイルはすでにあるので、このコマンドは不要
+# Emacs
+deploy 'l' $HOME/.emacs.d $EMACSD
+deploy 't' $EMACSD/.scratch-log
+deploy 't' $EMACSD/.scratch-log-prev
+## Cask
+### Caskファイルはすでにあるので、cask init は不要
+cd $EMACSD
 ./.cask/bin/cask
+
+# zsh
+deploy 'l' $HOME/.zshrc $ZSH/.zshrc
+deploy 't' $HOME/.zlogin
+echo "export PATH=$EMACSD/.cask/bin:$PATH" >> $HOME/.zlogin
+
+# Tmux
+deploy 'l' $HOME/.tmux.conf $TMUX/.tmux.conf
+
+# Git
+deploy 'l' $HOME/.gitconfig $GIT/.gitconfig
+
+# bin
+deploy 'm' $HOME/bin
+deploy 'l' $HOME/bin/loadaverage.sh $TMUX/loadaverage.sh
+deploy 'l' $HOME/bin/used_mem.sh $TMUX/used_mem.sh
+
+## Misc.
+deploy 'l' $HOME/.inputrc $MISC/.inputrc
+deploy 'l' $HOME/.screenrc $MISC/.screenrc
