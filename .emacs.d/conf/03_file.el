c42c78881d1b0976bfb1cedd21f4b5a904ac3431
Ikuya Fukumoto <ikuya.fukumoto@gmail.com>
2025-04-18 01:56:44 +0900

    emacs: 開いているファイルのリネーム c-x c-w

--- .emacs.d/conf/03_file.el
+++ .emacs.d/conf/03_file.el
@@ -11,3 +11,20 @@
 ; http://d.hatena.ne.jp/wadap/20120415/1334468285
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+
+
+;; 開いているファイルのリネーム
+(defun rename-current-buffer-file (new-name)
+  "Rename current buffer and file it is visiting to NEW-NAME."
+  (interactive "FNew name: ")
+  (let ((old-name (buffer-file-name)))
+    (if (not old-name)
+        (message "Buffer is not visiting a file!")
+      (when (get-buffer new-name)
+        (kill-buffer new-name))
+      (rename-file old-name new-name 1)
+      (set-visited-file-name new-name)
+      (set-buffer-modified-p nil)
+      (message "Renamed '%s' to '%s'" old-name new-name))))
+
+(define-key global-map (kbd "C-x C-w") 'rename-current-buffer-file)
